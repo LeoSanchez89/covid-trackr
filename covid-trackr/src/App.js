7b8fe999f6c48fbcb438d115eefa85e33bfc0a43
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-09-15 18:23:34 -0400

    dep added, card totals created

--- covid-trackr/src/App.js
+++ covid-trackr/src/App.js
@@ -2,28 +2,47 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Chart from "./charts/Chart";
 import axios from "axios";
+import ParticlesBg from "particles-bg";
+import Icon from "./charts/icon";
 
 function App() {
 	const [global, setGlobal] = useState({});
-  const [countries, setCountries] = useState([]);
+	const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    axios
+	let config = {
+		num: [4, 7],
+		rps: 1,
+		radius: [5, 55],
+		life:  [1.5,5],
+		v: 1,
+		tha: [-40, 40],
+		alpha: [.7, .1],
+		scale: [.03, .07],
+		body: Icon,
+		position: "all",
+		// color: "random",
+		cross: "dead",
+		random: 16,
+	};
+
+	useEffect(() => {
+		axios
 			.get("https://api.covid19api.com/summary")
 			.then((res) => {
 				// console.log("response", res.data);
-        setGlobal(res.data.Global);
-        setCountries(res.data.Countries)
+				setGlobal(res.data.Global);
+				setCountries(res.data.Countries);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
-  },[])
-	
+	}, []);
+
 	return (
 		<div className="App">
+			{/* <ParticlesBg type="custom" config={config} bg={true} /> */}
 			<h1>Daily Covid Trackr</h1>
-      <Chart global={global} countries={countries}/>
+			<Chart global={global} countries={countries} />
 		</div>
 	);
 }
--- covid-trackr/src/charts/Chart.js
+++ covid-trackr/src/charts/Chart.js
@@ -1,26 +1,44 @@
 import React from "react";
+import { Card, Button, CardTitle, CardText, CardFooter } from "reactstrap";
 
 function Chart({ global, countries }) {
-    // console.log(countries[179]);
-    // countries.map(country => {
-    //     console.log(`${country.Country}, Total Deaths: ${country.TotalDeaths}` )
-    // })
-
+	// console.log(countries[179]);
+	// countries.map(country => {
+	//     console.log(`${country.Country}, Total Deaths: ${country.TotalDeaths}` )
+	// })
+	console.log(global);
 	return (
-		<div>
-			<h2>
-				Global Total <span style={{ color: "gold" }}>Confirmed</span> Cases:{" "}
-				{global.TotalConfirmed}
-			</h2>
-			<h2>
-				Global Total <span style={{ color: "red" }}>Deaths</span>:{" "}
-				{global.TotalDeaths}
-			</h2>
-			<h2>
-				Global Total <span style={{ color: "limegreen" }}>Recovered</span>:{" "}
-				{global.TotalRecovered}
-			</h2>
-		</div>
+		<section>
+			<div class="card-wrapper">
+				<Card class="cases-card" id="new-cases">
+					<CardTitle>
+						<h2>New Cases</h2>
+					</CardTitle>
+					<CardText>
+						<h3 style={{ color: "darkorange" }}>+{global.NewConfirmed}</h3>
+					</CardText>
+					<CardFooter>Total: {global.TotalConfirmed}</CardFooter>
+				</Card>
+				<Card class="cases-card" id="new-deaths">
+					<CardTitle>
+						<h2>Deaths</h2>
+					</CardTitle>
+					<CardText>
+						<h3 style={{ color: "red" }}>+{global.NewDeaths}</h3>
+					</CardText>
+					<CardFooter>Total: {global.TotalDeaths}</CardFooter>
+				</Card>
+				<Card class="cases-card" id="new-recovered">
+					<CardTitle>
+						<h2>Recovered</h2>
+					</CardTitle>
+					<CardText>
+						<h3 style={{ color: "limegreen" }}>+{global.NewRecovered}</h3>
+					</CardText>
+					<CardFooter>Total: {global.TotalRecovered}</CardFooter>
+				</Card>
+			</div>
+		</section>
 	);
 }
 
